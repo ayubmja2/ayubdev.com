a425e4a74a217df541b440c83411d5492fa10aea
Ayub Ali <ayubmja@icloud.com>
2023-06-01 23:11:14 +0000

    added public folder for static items and modified index-ws and index.html

--- index-ws.js
+++ index-ws.js
@@ -3,9 +3,15 @@ const server = require('http').createServer();
 const app = express();
 const PORT = 3000;
 
+
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname});
 });
+
 
 server.on('request', app);
 
