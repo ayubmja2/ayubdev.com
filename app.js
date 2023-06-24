798018cf61c54574dc377f74eb788145783ace9d
Ayub <ayub@localhost.localdomain>
2023-06-24 04:58:31 -0400

    deleted index-ws.js and created app.js instead better now than later

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const http = require('http');
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+const PORT = 3000;
 
-http.createServer(function(req, res){
-	res.write("On the way on being a full stack engineer");
-	res.end();
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname, {extensions: ['html']}));
 
-}).listen(3000);
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile('/images/favicon_io/favicon-32x32.png');
+});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-console.log("Server Started");
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/mprojects', function (req, res) {
+  res.sendFile(__dirname + '/mprojects.html');
+});
+
+server.on('request', app);
+
+server.listen(PORT, function () {
+  console.log('Listening on ' + PORT);
+});
